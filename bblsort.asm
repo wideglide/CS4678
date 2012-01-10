d7a653b9cb49cc1aaa09494afea40b5aa0f97636
wideglide <joshua.bundt@gmail.com>
2012-01-09 22:00:54 -0800

    assignment 1

---
+++ bblsort.asm
@@ -0,0 +1,45 @@
+;nasm -f elf bblsort.asm
+;ld -o sort bblort.o
+
+USE32
+section	.data
+
+global	passes
+
+array:		dd	9,5,8,3,0,2,1,7,6,4
+passes:		dd	0
+
+
+section	.text
+			; we must export the entry point to the ELF linker or
+global	_start		; loader. They conventionally recognize _start as their
+			; entry point. Use ld -e foo to override the default.
+
+_start:
+	mov	edx, 9
+
+outerloop:
+	mov	ecx, 0
+	mov	edi, 1			; $edi test if swaps are made
+innerloop:
+	mov	eax, [array + ecx *4]
+	mov 	ebx, [array +4 + ecx * 4]
+	cmp	eax, ebx
+	jge	next
+	inc	edi			; swapping, so increment $edi
+	mov	[array + ecx *4], ebx
+	mov	[array + 4 + ecx * 4], eax
+next:
+	inc	ecx
+	cmp	ecx, edx
+	jl	innerloop
+endinner:
+	inc	DWORD [passes]
+	dec	edi			; if $edi is zero we're done
+	jz	done
+	dec	edx
+	jnz	outerloop
+done:
+	mov	ebx, DWORD [passes]	; first syscall argument: exit code
+	mov	eax, 1			; system call number (sys_exit)
+	int	0x80			; call kernel
